db2818561f1485286959713faaf2e9955ba447ed
Milan <rajsite@gmail.com>
2014-08-09 20:21:01 -0500

    Removed AIAA, bootloader.js, evil. Added GA
    Removed references to aiaa, removed bootloader.js and replaced function
    usage, removed all eval / evil, added Google Analytics to default
    template

--- js/script.js
+++ js/script.js
@@ -1,137 +1,135 @@
-/*global aiaa, $*/
+/*global $*/
 
-// (function (aiaa) { aiaa.poopytoo = "hello"; } (aiaa));
-
-
-
-//global variables
-aiaa.konamiEnabled = 'false';
-aiaa.navLastLink = '';
-
-//global ajax error handler
-$(document).ajaxError(function (e, xhr, settings, exception) {
+(function () {
     'use strict';
-	//check if the ajax error may have been from the nav loader
-	if (aiaa.navLastLink === settings.url) {
-		$("td.scroller").stop(true, true).fadeTo("fast", 1);
-	}
-	//alert('error in: ' + settings.url + ' \\n'+'error:\\n' + exception);
-});
+    var konamiEnabled = 'false',
+        navLastLink = '';
 
-// function loadNewScrollerFromLink
-// param: link
-// usage: takes the given link, dynamically loads that page and searches for the scoller content on that page
-//        the data from the loaded page scroller is then formatted and injected into this page scroller
-// ASSUMPTIONS: This script ONLY works if all the linked pages are in the SAME level directory, NO SUB DIRECTORIES
-aiaa.loadNewScrollerFromLink = function (link) {
-    'use strict';
-	//fade out the scroller before loading the directory
-	$("td.scroller").fadeTo("fast", 0);
+    //global ajax error handler
+    $(document).ajaxError(function (e, xhr, settings, exception) {
+        //check if the ajax error may have been from the nav loader
+        if (navLastLink === settings.url) {
+            $("td.scroller").stop(true, true).fadeTo("fast", 1);
+        }
+        //alert('error in: ' + settings.url + ' \\n'+'error:\\n' + exception);
+    });
 
-	//get the linked page and start processing
-    $.get(link, function (responseText, textStatus) {
-		//Load the scroller text and inject in this page
-		//since all files in the same directory, routing issues shouldn't happen
-		$("#scroller").html($("#scroller", responseText).html());
-		
-		//done loading so fade back in!
-		$("td.scroller").stop(true, true).fadeTo("fast", 1);
-	});
-};
+    // function loadNewScrollerFromLink
+    // param: link
+    // usage: takes the given link, dynamically loads that page and searches for the scoller content on that page
+    //        the data from the loaded page scroller is then formatted and injected into this page scroller
+    // ASSUMPTIONS: This script ONLY works if all the linked pages are in the SAME level directory, NO SUB DIRECTORIES
+    function loadNewScrollerFromLink(link) {
+        //fade out the scroller before loading the directory
+        $("td.scroller").fadeTo("fast", 0);
 
+        //get the linked page and start processing
+        $.get(link, function (responseText, textStatus) {
+            //Load the scroller text and inject in this page
+            //since all files in the same directory, routing issues shouldn't happen
+            $("#scroller").html($("#scroller", responseText).html());
 
-//run when the document object mmodel has loaded
-$(document).ready(function () {
-    'use strict';
-	//Activate the navbar scroller script
-	$("td.nav, td.navjoin").hoverIntent({
-		sensitivity: 7, // number = sensitivity threshold (must be 1 or higher)    
-		interval: 45, // number = milliseconds for onMouseOver polling interval    
-		over: function () {  // function = onMouseOver callback (REQUIRED)    
-			//Get the hovered link
-			var link = $(this).children("a").attr("href");
-			
-			//check if the link is the same as the last one, 
-			//assumes loaded correctly last time so don't load again
-			if (link !== aiaa.navLastLink) {
-				//First set the last link for the error handler
-				aiaa.navLastLink = link;
-				//Then try to load the file
-				aiaa.loadNewScrollerFromLink(link);
-			}
-		},
-		timeout: 100, // number = milliseconds delay before onMouseOut    
-		out: function () { // function = onMouseOut callback (REQUIRED) 
-		//empty function
-		}
-	});
-	
-	//make each navbar cell clickable instead of just the test link
-	$('td.nav, td.navjoin').click(function () {
-		document.location = $(this).children('a').attr('href');
-	});
-	
-	//ie hover fix for nav bar
-	$('td.nav').hover(function () {
-		$(this).addClass('td-nav-ie-hover');
-		$(this).children('a').removeClass('nav');
-		$(this).children('a').addClass('a-nav-ie-hover');
-	}, function () {
-		$(this).removeClass('td-nav-ie-hover');
-		$(this).children('a').addClass('nav');
-		$(this).children('a').removeClass('a-nav-ie-hover');
-	});
-
-	//ie hover fix for nav bar join button
-	$('td.navjoin').hover(function () {
-		$(this).addClass('td-navjoin-ie-hover');
-		$(this).children('a').removeClass('navjoin');
-		$(this).children('a').addClass('a-navjoin-ie-hover');
-	}, function () {
-		$(this).removeClass('td-navjoin-ie-hover');
-		$(this).children('a').addClass('navjoin');
-		$(this).children('a').removeClass('a-navjoin-ie-hover');
-	});
+            //done loading so fade back in!
+            $("td.scroller").stop(true, true).fadeTo("fast", 1);
+        });
+    }
 
 
-	//gallery fade effect
-	$('p.gallery').children().hover(function () {
-		$(this).siblings().stop().fadeTo(500, 0.5);
-	}, function () {
-		$(this).siblings().stop().fadeTo(500, 1);
-	});
+    //run when the document object mmodel has loaded
+    $(document).ready(function () {
+        //Activate the navbar scroller script
+        $("td.nav, td.navjoin").hoverIntent({
+            sensitivity: 7, // number = sensitivity threshold (must be 1 or higher)    
+            interval: 45, // number = milliseconds for onMouseOver polling interval    
+            over: function () {  // function = onMouseOver callback (REQUIRED)    
+                //Get the hovered link
+                var link = $(this).children("a").attr("href");
 
-	
-	//valid css and xhtml button effect
-	//Note: if a script changes the rel attr of the links, then functionality will break
-	$('a.hover').children('img').each(function (index, obj) {
-	
-		//save the original image url
-		$(obj).parent().attr('rel', $(obj).attr('src'));
-	
-		//make the hover element
-		$(obj).hover(function () {
-			$(this).attr('src', $(this).attr('src').replace('-hover', ''));
-		}, function () {
-			$(this).attr('src', $(this).parent().attr('rel'));
-		});
-	});
-	
+                //check if the link is the same as the last one, 
+                //assumes loaded correctly last time so don't load again
+                if (link !== navLastLink) {
+                    //First set the last link for the error handler
+                    navLastLink = link;
+                    //Then try to load the file
+                    loadNewScrollerFromLink(link);
+                }
+            },
+            timeout: 100, // number = milliseconds delay before onMouseOut    
+            out: function () { // function = onMouseOut callback (REQUIRED) 
+            //empty function
+            }
+        });
 
-	//secret konami code ;)
-	aiaa.konamiEnabled = $.cookie('konamiEnabled');
-	if (aiaa.konamiEnabled === 'true') {
-		$('body').css('background-image', $('body').css('background-image').replace('background', 'backgroundk'));
-        $('body').css('background-color', 'black');
-	}
-	$(window).konami({cheat: function () {
-		$('body').css('background-image', $('body').css('background-image').replace('background', 'backgroundk'));
-        $('body').css('background-color', 'black');
-		$.cookie('konamiEnabled', 'true');
-    }});
-	$(window).konami({cheat: function () {
-		aiaa.loadScript(null, 'js/asteroids.min.js');
-	}});
-	
+        //make each navbar cell clickable instead of just the test link
+        $('td.nav, td.navjoin').click(function () {
+            document.location = $(this).children('a').attr('href');
+        });
 
-});
+        //ie hover fix for nav bar
+        $('td.nav').hover(function () {
+            $(this).addClass('td-nav-ie-hover');
+            $(this).children('a').removeClass('nav');
+            $(this).children('a').addClass('a-nav-ie-hover');
+        }, function () {
+            $(this).removeClass('td-nav-ie-hover');
+            $(this).children('a').addClass('nav');
+            $(this).children('a').removeClass('a-nav-ie-hover');
+        });
+
+        //ie hover fix for nav bar join button
+        $('td.navjoin').hover(function () {
+            $(this).addClass('td-navjoin-ie-hover');
+            $(this).children('a').removeClass('navjoin');
+            $(this).children('a').addClass('a-navjoin-ie-hover');
+        }, function () {
+            $(this).removeClass('td-navjoin-ie-hover');
+            $(this).children('a').addClass('navjoin');
+            $(this).children('a').removeClass('a-navjoin-ie-hover');
+        });
+
+
+        //gallery fade effect
+        $('p.gallery').children().hover(function () {
+            $(this).siblings().stop().fadeTo(500, 0.5);
+        }, function () {
+            $(this).siblings().stop().fadeTo(500, 1);
+        });
+
+
+        //valid css and xhtml button effect
+        //Note: if a script changes the rel attr of the links, then functionality will break
+        $('a.hover').children('img').each(function (index, obj) {
+
+            //save the original image url
+            $(obj).parent().attr('rel', $(obj).attr('src'));
+
+            //make the hover element
+            $(obj).hover(function () {
+                $(this).attr('src', $(this).attr('src').replace('-hover', ''));
+            }, function () {
+                $(this).attr('src', $(this).parent().attr('rel'));
+            });
+        });
+
+
+        //secret konami code ;)
+        konamiEnabled = $.cookie('konamiEnabled');
+        if (konamiEnabled === 'true') {
+            $('body').css('background-image', $('body').css('background-image').replace('background.png', 'backgroundk.jpg'));
+            $('body').css('background-color', 'black');
+        }
+        $(window).konami({cheat: function () {
+            $('body').css('background-image', $('body').css('background-image').replace('background.png', 'backgroundk.jpg'));
+            $('body').css('background-color', 'black');
+            $.cookie('konamiEnabled', 'true');
+        }});
+        $(window).konami({cheat: function () {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'js/asteroids.min.js';
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }});
+
+
+    });
+}());
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,45 +1,8 @@
-/*global aiaa, $*/
-/*jslint evil: true*/
+/*global $*/
 /*******************************************
 * Script from http://www.queness.com/post/1450/jquery-photo-slide-show-with-slick-caption-tutorial-revisited
 *******************************************/
 
-function slideShow(speed) {
-    'use strict';
-	//append a LI item to the UL list for displaying caption
-	$('ul.slideshow').append('<li id="slideshow-caption" class="caption"><div class="slideshow-caption-container"><h3></h3><p></p></div></li>');
-
-	//Set the opacity of all images to 0
-	$('ul.slideshow li').css({opacity: 0.0});
-	
-	//Get the first image and display it (set it to full opacity)
-	$('ul.slideshow li:first').css({opacity: 1.0});
-	
-	//Get the caption of the first image from REL attribute and display it
-//	$('#slideshow-caption h3').html($('ul.slideshow a:first').find('img').attr('title'));
-//	$('#slideshow-caption p').html($('ul.slideshow a:first').find('img').attr('alt'));
-	$('#slideshow-caption h3').html($('ul.slideshow li:first').find('img').attr('title'));
-	$('#slideshow-caption p').html($('ul.slideshow li:first').find('img').attr('alt'));
-		
-	//Display the caption
-	$('#slideshow-caption').css({opacity: 0.7, bottom: 0});
-	
-	//Call the gallery function to run the slideshow	
-	var timer = setInterval('gallery()', speed);
-	
-	//pause the slideshow on mouse over
-/*
-	$('ul.slideshow').hover(
-		function () {
-			clearInterval(timer);
-        },
-		function () {
-			timer = setInterval('gallery()',speed);			
-		}
-	);
-*/
-	
-}
 
 function gallery() {
     'use strict';
@@ -69,10 +32,46 @@ function gallery() {
 	current.animate({opacity: 0.0}, 1000).removeClass('show');
 }
 
-aiaa.loadScript('jQuery', null, false, function () {
+function slideShow(speed) {
     'use strict';
-    $(document).ready(function () {
-        //Execute the slideShow, set 4 seconds for each images
-        slideShow(7000);
-    });
+	//append a LI item to the UL list for displaying caption
+	$('ul.slideshow').append('<li id="slideshow-caption" class="caption"><div class="slideshow-caption-container"><h3></h3><p></p></div></li>');
+
+	//Set the opacity of all images to 0
+	$('ul.slideshow li').css({opacity: 0.0});
+	
+	//Get the first image and display it (set it to full opacity)
+	$('ul.slideshow li:first').css({opacity: 1.0});
+	
+	//Get the caption of the first image from REL attribute and display it
+//	$('#slideshow-caption h3').html($('ul.slideshow a:first').find('img').attr('title'));
+//	$('#slideshow-caption p').html($('ul.slideshow a:first').find('img').attr('alt'));
+	$('#slideshow-caption h3').html($('ul.slideshow li:first').find('img').attr('title'));
+	$('#slideshow-caption p').html($('ul.slideshow li:first').find('img').attr('alt'));
+		
+	//Display the caption
+	$('#slideshow-caption').css({opacity: 0.7, bottom: 0});
+	
+	//Call the gallery function to run the slideshow	
+	var timer = setInterval(function () { gallery(); }, speed);
+	
+	//pause the slideshow on mouse over
+/*
+	$('ul.slideshow').hover(
+		function () {
+			clearInterval(timer);
+        },
+		function () {
+			timer = setInterval('gallery()',speed);			
+		}
+	);
+*/
+	
+}
+
+
+$(document).ready(function () {
+    'use strict';
+    //Execute the slideShow, set 4 seconds for each images
+    slideShow(7000);
 });
